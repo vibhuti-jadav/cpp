79c340e652475187487e08cb8f1ffdfdb97b19d1
vibhuti-jadav <vibhutij109@gmail.com>
2024-11-18 15:53:57 +0530

    first day

---
+++ day1/oprator/intro.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std ;
+
+ int main()
+{
+  int a=50;
+   int b=5;
+
+   cout << "addition of -> : " << a+b <<endl;
+   cout << "subsciption of -> : " << a-b <<endl;
+   cout << "division of -> : " << a/b <<endl;
+   cout << "multiplication of -> : " << a*b <<endl;
+   cout << "module of -> : " << a%b <<endl;
+
+   
+
+}
